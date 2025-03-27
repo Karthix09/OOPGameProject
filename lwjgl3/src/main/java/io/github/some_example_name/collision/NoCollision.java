ae48ffdb76b7c47aab93be3fbaa4c012b583cafc
md-zharfan <2402959@sit.singaporetech.edu.sg>
2025-03-27 17:35:17 +0800

    Updated NoCollision

--- lwjgl3/src/main/java/io/github/some_example_name/collision/NoCollision.java
+++ lwjgl3/src/main/java/io/github/some_example_name/collision/NoCollision.java
@@ -1,19 +1,22 @@
 package io.github.some_example_name.collision;
 
+import java.util.List;
 import io.github.some_example_name.entities.Entity;
 
-public class NoCollision{
-     
-      public boolean checkCollision(Entity e1, Entity e2) {
-          return false;
-      }
-      
-     
-      public void detectCollision(Entity e1, Entity e2) {
-          // No collision handling
-      }
+public class NoCollision extends CollisionManager {
 
-      public void dispose() {
-    	  
-      }
+    @Override
+    public boolean checkCollision(Entity e1, Entity e2) {
+        return false;
+    }
+
+    @Override
+    public void detectCollision(List<Entity> entities) {
+        // No collision logic, can be implemented for future games
+    }
+
+    @Override
+    public void dispose() {
+        // No resources to clean up
+    }
 }
